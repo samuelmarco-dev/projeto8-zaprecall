40ddb3ee6fa727225213dc14a985cd537d059660
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-04-24 11:30:05 -0300

    estrutura tela de deck

--- src/components/TelaDeck/index.js
+++ src/components/TelaDeck/index.js
@@ -1,14 +1,20 @@
 // import { useState } from "react";
 // import arrFlashCards from "../../data/dados";
-
-// import LogoPequeno from "./../../assets/img/logo-pequeno.png";
 // import Flashcard from "./../Flashcard";
 // import Icone from "./../Icone";
+import LogoPequeno from "./../../assets/img/logo-pequeno.png";
 import "./style.css";
 
 function TelaDeck(){
     return (
-        <h1>Deck</h1>
+        <div className="Deck">
+            <header>
+                <img src={LogoPequeno} alt="ZapRecall" />
+                <h1>Zap Recall</h1>
+            </header>
+            <main></main>
+            <footer></footer>
+        </div>
     )
 }
 
